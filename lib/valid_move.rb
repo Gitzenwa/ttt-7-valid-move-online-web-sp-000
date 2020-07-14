a526ca89db3dff18e0db77533197757efbcb42da
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-14 18:55:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,14 @@
 # code your #valid_move? method here
 
-#method accepts two arguments
+#method accepts two arguments 
 def valid_move? (board, index)
-  #if statement to see if the position is taken
+  #if statement to see if the position is taken 
   #when the position is taken, return false
-  if position_taken?
+  if position_taken? 
     return false
   else
     true
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
